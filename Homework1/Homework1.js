de8fc16b81222eafab026134a9eb8c85fc92a313
Danacious <dee7@students.calvin.edu>
2016-10-28 19:53:21 -0400

    Update Homework1.js
    Fixed some fundamental issues and added route parameters.

--- Homework1/Homework1.js
+++ Homework1/Homework1.js
@@ -4,20 +4,18 @@
 var express = require('express');
 var app = express();
 
-function Person(firstName, lastName, loginID, startDate){
+function Person(firstName, lastName, startDate){
    this.firstName = firstName;
    this.lastName = lastName;
-   this.loginID = loginID;
    this.startDate = startDate;
 }
 
 //Hard code some people objects
-var person1 = new Person("Dan", "Ermer", "Danacious", "06/08/1995");
-var person2 = new Person("Joe", "Bob", "1234", "10/3/2016");
-var person3 = new Person("Sarah", "Evans", "Star", "11/23/2015");
-
-//Make a list of those people
-var peopleList = [person1, person2, person3];
+var people = {
+   "dee7": new Person("Dan", "Ermer", "06/08/1995"),
+   "Ten": new Person("Joe", "Bob", "10/3/2016"),
+   "Star": new Person("Sarah", "Evans", "11/23/2015")
+}
 
 //Listen for port 3000
 app.listen(3000, function () {
@@ -26,82 +24,34 @@ app.listen(3000, function () {
 
 // Bring of a full list of people
 app.get('/people', function (req, res) {
-   res.json(peopleList);
+   res.json(people);
 });
 
 // Full records by loginID
-app.get('/people/Danacious', function (req, res) {
-  for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "Danacious"){
-        res.json(peopleList[i]);
+app.get('/people/:id', function (req, res) {
+    if (req.params.id in people){
+        res.json(people[req.params.id]);
      }
      else{
         res.sendStatus(404);
      }
-  }
-});
-
-app.get('/people/1234', function (req, res) {
-  for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "1234"){
-        res.json(peopleList[i]);
-     }
-     else{
-        res.sendStatus(404);
-     }
-  }
-});
-
-app.get('/people/Star', function (req, res) {
-  for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "Star"){
-        res.json(peopleList[i]);
-     }
-     else{
-        res.sendStatus(404);
-     }
-  }
 });
 
 //First and last names by ID
-app.get('/people/Danacious/name', function (req, res) {
-  for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "Danacious"){
-        res.json("The name of this person is: " + peopleList[i].firstName + " " + peopleList[i].lastName);
+app.get('/people/:id/name', function (req, res) {
+     if (req.params.id in people){
+        res.json("The name of this person is: " + people[req.params.id].firstName + " " + people[req.params.id].lastName);
      }
      else{
         res.sendStatus(404);
      }
-  }
-});
-
-app.get('/people/1234/name', function (req, res) {
-  for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "1234"){
-        res.json("The name of this person is: " + peopleList[i].firstName + " " + peopleList[i].lastName);
-     }
-     else{
-        res.sendStatus(404);
-     }
-  }
-});
-
-app.get('/people/Star/name', function (req, res) {
-  for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "Star"){
-        res.json("The name of this person is: " + peopleList[i].firstName + " " + peopleList[i].lastName);
-     }
-     else{
-        res.sendStatus(404);
-     }
-  }
 });
 
 //Years in the company by ID
-app.get('/people/Danacious/years', function (req, res) {
+app.get('/people/:id/years', function (req, res) {
     for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "Danacious"){
-        res.json("This person has been in the company for 21 years");
+     if (req.params.id in people){
+        res.json(getAge(people[req.params.id].startDate));
      }
      else{
         res.sendStatus(404);
@@ -109,26 +59,14 @@ app.get('/people/Danacious/years', function (req, res) {
   }
 });
 
-app.get('/people/1234/years', function (req, res) {
-    for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "1234"){
-        res.json("This person has been in the company for 0 years");
-     }
-     else{
-        res.sendStatus(404);
-     }
-  }
-});
-
-app.get('/people/Star/years', function (req, res) {
-    for (i = 0; i < 3; i++){
-     if (peopleList[i].loginID == "Star"){
-        res.json("This person has been in the company for 1 years");
-     }
-     else{
-        res.sendStatus(404);
-     }
-  }
-});
-
-
+//Function for finding the number of years between two dates
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
