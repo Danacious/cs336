80a3f3d7b782d4be778523e4eaf5a54fcc904da9
Daniel Ermer <dee7@thompson.cs.calvin.edu>
2016-11-16 20:28:39 -0500

    Finished Homework3

--- Homework3/server.js
+++ Homework3/server.js
@@ -1,11 +1,14 @@
 //Daniel Ermer
-//CS 336 - Homework 2
+//CS 336 - Homework 3
 
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var MongoClient = require('mongodb').MongoClient
+
+var db;
 
 var COMMENTS_FILE = path.join(__dirname, 'peopleList.json');
 
@@ -15,6 +18,56 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Homework3
+
+//Most, if not all, code very heavily influenced by labs 8 and 10
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+MongoClient.connect('mongodb://cs336:bjarne@ds053295.mlab.com:53295/cs336', function (err, db) {
+  if (err) throw err
+
+  db.collection('people').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  })
+})
+
+app.get('/api/people', function(req, res) {
+    db.collection("people").find({}).toArray(function(err, docs) {
+        if (err) throw err;
+        res.json(docs);
+    });
+});
+
+app.post('/api/people', function(req, res) {
+    var newPerson = {
+        id: req.body.id,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        startDate: req.body.startDate,
+    };
+    db.collection("people").insertOne(newPerson, function(err, result) {
+        if (err) throw err;
+        var newId = result.insertedId;
+        db.collection("people").find({}).toArray(function(err, docs) {
+            if (err) throw err;
+            res.json(docs);
+        });
+    });
+});
+
+var mongoURL = 'mongodb://cs336:bjarne@ds053295.mlab.com:53295/cs336';
+MongoClient.connect(mongoURL, function(err, dbConnection) {
+    if (err) throw err;
+    db = dbConnection;
+});
+
+//Homework1 and 2
 function Person(firstName, lastName, startDate){
    this.firstName = firstName;
    this.lastName = lastName;
@@ -140,52 +193,6 @@ app.post('/search', function(req, res){
       foundName = "Couldn't find this person";
    }
    res.json(foundName);
-});
-
-// Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
-
-
-app.get('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    res.json(JSON.parse(data));
-  });
-});
-
-app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    var people = JSON.parse(data);
-    var newPerson = {
-      id: req.body.code,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      startDate: req.body.startDate,
-    };
-    people.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.json(people);
-    });
-  });
 });
 
 
